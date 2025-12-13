01ebe02a8ce3713360b934f240f4ff1deb48c7d4
nadeesha gehani <nadeesha16gehani@gmail.com>
2025-12-14 00:19:33 +0530

    added form

--- src/view/Manage_Doctors.java
+++ src/view/Manage_Doctors.java
@@ -1,20 +1,35 @@
 package view;
 
-
+import DataBase.database;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
-import DataBase.database;
-
+import java.util.List;
 
 
 public class Manage_Doctors extends JFrame {
     private JLabel lblname;
-    private JTextField txtname,txtavailable,txtspecialty;
+    private JTextField txtname,txtspecialty;
     private JLabel lblspecialty ,lblavailable,lbltimeslots;
-    private JComboBox timesslotscombobox;
     private JPanel ManageDoctorsPane;
+    private JList list1;
+    private JButton btnadd;
+    private JComboBox comboxavailable;
+    private JCheckBox monday090011CheckBox;
+    private JCheckBox monday110013CheckBox;
+    private JCheckBox monday140016CheckBox;
+    private JCheckBox tuesday090011CheckBox;
+    private JCheckBox tuesday110013CheckBox;
+    private JCheckBox tuesday140016CheckBox;
+    private JCheckBox wednesday090011CheckBox;
+    private JCheckBox wednesday110013CheckBox;
+    private JCheckBox wednesday140016CheckBox;
+    private JCheckBox thursday090011CheckBox;
+    private JCheckBox thursday110013CheckBox;
+    private JCheckBox thursday140016CheckBox;
+    private JCheckBox friday090011CheckBox;
+    private JCheckBox friday090011CheckBox1;
+    private JCheckBox friday140016CheckBox;
     private database DB;
 
     Manage_Doctors(){
@@ -24,13 +39,28 @@ public class Manage_Doctors extends JFrame {
         setSize(600,550);
         setLocationRelativeTo(null);
         setVisible(true);
+
+        btnadd.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String selectedValue = (String) comboxavailable.getSelectedItem();
+                DB.addDoctor(
+                        txtname.getText(),
+                        txtspecialty.getText(),
+                        Boolean.parseBoolean(selectedValue),
+                        List.of()
+                );
+
+            }
+        });
     }
 
 
 
     static void main() {
+        database DB = new database();
         new Manage_Doctors();
-        database DBDB = new database();
-        DBDB.addPatient( "Sunil Perera", 35, "Male", "Fever");
+
+
     }
 }
