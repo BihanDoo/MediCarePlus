1e64a0abc5481ce84d6ebaa2410afdbca6c5f839
ApexninjaYT <amhapugaswatte@gmail.com>
2025-12-13 23:42:38 +0530

    Patient form

--- src/view/main.java
+++ src/view/main.java
@@ -3,7 +3,6 @@ package view;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import view.PatientManagementForm;
 
 
 public class main extends JFrame {
